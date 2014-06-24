6b6915a8c14dd7ba5219f923565f97a208a058ce
Philip Corliss <pcorliss@50projects.com>
2014-06-23 20:42:30 -0500

    Added Enabling feature

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -1,11 +1,12 @@
 class DevsController < ApplicationController
   def index
+    @enabled = enabled?
     @devs = Dev.all.order(:id => :desc)
     @dev = Dev.new
   end
 
   def create
-    Dev.create(dev_params)
+    Dev.create(dev_params) if enabled?
     redirect_to devs_path
   end
 
@@ -13,5 +14,14 @@ class DevsController < ApplicationController
 
   def dev_params
     params.require(:dev).permit(:name, :description, :url)
+  end
+
+  def enabled?
+    cache = Dalli::Client.new
+    features = cache.fetch('features', 15) do
+      response = Net::HTTP.get_response(URI.parse('http://vuln.alttab.org/features'))
+      JSON.parse(response.body)
+    end
+    features['step3']
   end
 end
